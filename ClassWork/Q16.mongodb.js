280a0ea2a9db8e73f6382c2b9d0c74b09d36a898
vishal1238 <webervishal@gmail.com>
2026-02-04 15:50:00 +0530

    Q16 update

---
+++ ClassWork/Q16.mongodb.js
@@ -0,0 +1,124 @@
+db.orders.aggregate([
+  {
+    $project: {
+      _id: 0,
+      empid: 1,
+      orderValue: 1,
+    },
+  },
+]);
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "employees",
+      localField: "empid",
+      foreignField: "_id",
+      as: "users",
+    },
+  },
+]);
+
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",
+      localField: "_id",
+      foreignField: "empid",
+      as: "orders",
+    },
+  },
+]);
+
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",
+      localField: "_id",
+      foreignField: "empid",
+      as: "orders",
+    },
+  },
+  { $unwind: "$orders" },
+  {
+    $project: {
+      name: 1,
+      product: "$orders.product",
+      orderValue: "$orders.orderValue",
+    },
+  },
+]);
+
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",
+      let: { uid: "$_id" },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$empid", "$$uid"] },
+          },
+        },
+      ],
+      as: "orders",
+    },
+  },
+  {
+    $project: {
+      name: 1,
+      product: "$orders.product",
+      orderValue: "$orders.orderValue",
+    },
+  },
+]);
+
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",
+      let: { uid: "$_id" },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$empid", "$$uid"] },
+          },
+        },
+      ],
+      as: "orders",
+    },
+  },
+  { $unwind: "$orders" },
+  {
+    $project: {
+      name: 1,
+      product: "$orders.product",
+      orderValue: "$orders.orderValue",
+    },
+  },
+]);
+
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",
+      let: { uid: "$_id" },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$empid", "$$uid"] },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            empid: 1,
+            product: 1,
+            orderValue: 1,
+          },
+        },
+      ],
+      as: "orders",
+    },
+  },
+]);
