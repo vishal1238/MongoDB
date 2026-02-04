0bd3009c46d4cfefc48a31794e757a4acad04216
vishal1238 <webervishal@gmail.com>
2026-02-04 12:37:45 +0530

    Q12 update

--- ClassWork/Q12.mongodb.js
+++ ClassWork/Q12.mongodb.js
@@ -1,27 +1,47 @@
-db.employees.aggregate(
-    [
-        {$match: {department: "HR"}},
-        {$project: {name: 1, email: 1}},
-        {$sort: {name: 1}},
-        {$limit: 3},
-        {$skip: 1}
-    ]
-)
+
+db.employees.aggregate([
+  { $match: { department: "HR" } },
+  { $project: { name: 1, email: 1 } },
+  { $sort: { name: 1 } },
+  { $limit: 3 },
+  { $skip: 1 },
+]);
 
 db.employees.aggregate(
     [
         {
-            $group: { _id: "$department", 
-            total: {$sum: "$salary"}
-             }
-        },
-        {$sort: {total: 1}},
-        {$limit: 3}
+            $group:{
+                _id:"$department",
+                total:{$sum:"$salary"}
+            }
+        }
     ]
-)
+);
+
 
 db.employees.aggregate(
     [
-        {$project: {_id: 0, name: 1, salary: 1, annualSal: {$multiply: [12, "$salary"]}}}
+        {
+            $group:{
+                _id:"$department",
+                Total:{$sum:"$salary"},
+                Highest:{$max:"$salary"},
+                Lowest:{$min:"$salary"},
+                Average:{$avg:"$salary"}
+            }
+        },
+    ]
+);
+
+
+db.employees.aggregate(
+    [
+        {$project:{
+                _id:0,
+                name:1,
+                salary:1,
+                annualSal:{$multiply:[12,"$salary"]}
+            }
+        }
     ]
 )
--- ClassWork/Q14.mongodb.js
+++ ClassWork/Q14.mongodb.js
@@ -2,3 +2,11 @@ db.employees.find(
     {},
     {name: 1, dept: "$department"}
 )
+
+db.users.insertOne(
+    {
+        name: "Yash",
+        age: 20,
+        
+    }
+)
