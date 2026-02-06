d7d252d848d4645a050be94f43fb8f8f4d90fda0
vishal1238 <webervishal@gmail.com>
2026-02-06 09:34:19 +0530

    q19 update

---
+++ ClassWork/Q19.mongodb.js
@@ -0,0 +1,20 @@
+db.createView("orderView","employees",
+    [
+        {$lookup:{
+            from:"orders",
+            localField:"_id",
+            foreignField:"empid",
+            as:"orders"
+        }},
+        {$unwind:"$orders"},
+        {$project:{
+            _id:0,
+            name:1,
+            email:1,
+            department:1,
+            product:"$orders.product",
+            orderValue:"$orders.orderValue"
+        }},
+
+    ]
+)
